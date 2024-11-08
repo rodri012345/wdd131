908fecb92a067585088855b093864ef1ffe196b1
Rodry <rodrigo.quena14@gmail.com>
2024-11-08 14:32:41 -0400

    The page has been finished

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("copyright").textContent = `© ${currentYear}`;
+
+const lastModified = document.lastModified;
+document.getElementById("last-modified").textContent = `Last Modification: ${lastModified}`;
