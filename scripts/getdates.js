e04ab8a2ccd905e74aa26ca57807f88b18c1adc7
Rodry <rodrigo.quena14@gmail.com>
2024-11-08 16:39:05 -0400

    More descriptions added

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,6 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright").textContent = `© ${currentYear}`;
 
+
 const lastModified = document.lastModified;
 document.getElementById("last-modified").textContent = `Last Modification: ${lastModified}`;
